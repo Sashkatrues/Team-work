a022bc78d6eb1eefc275597cd9c93f3e6412cd76
rnat200 <jmz3qqvt@tutamail.com>
2025-08-26 20:17:27 +0300

    snusaedcosplay

--- functions.cpp
+++ functions.cpp
@@ -19,10 +19,7 @@ void CheckInputFile(std::ifstream& fin)
 
 void CheckOutputFile(std::fstream& fout)
 {
-    if (!fout.good())
-    {
-        throw "file doesn't exist\n";
-    }
+  
     if (!fout)
     {
         throw "output file error\n";
@@ -61,6 +58,7 @@ void CreateStudentBinary(std::fstream& bin, std::string* array, int32_t size)
     for (int32_t i{}; i < size; ++i)
     {
         bin.write(array[i].c_str(),  array[i].length());
+        bin.write(" ", 1);
     }
 }
 
@@ -82,39 +80,9 @@ void CreateGradeBinary(std::fstream& bin, std::string* array, int32_t size)
     for (int32_t i{}; i < size; ++i)
     {
         bin.write(array[i].c_str(), array[i].length());
+        bin.write(" ", 1);
     }
     bin.close();
 }
 
-void TaskA (){
-    try
-    {
-        std::ifstream fin_s("list_of_students.txt");
-        CheckInputFile(fin_s);
-        std::ifstream fin_g("list_of_student_grades.txt");
-        CheckInputFile(fin_g);
-        std::fstream bin_s("students.bin", std::ios::in | std::ios::out | std::ios::binary);
-        CheckOutputFile(bin_s);
-        std::fstream bin_g("grades.bin", std::ios::in | std::ios::out | std::ios::binary);
-        CheckOutputFile(bin_g);
 
-        int32_t size{ CountStudents(fin_s) };
-        std::string* students = new std::string[size];
-        InputStudents(fin_s, students, size);
-        CreateStudentBinary(bin_s, students, size);
-
-        std::string* grades = new std::string[size];
-        InputGrades(fin_g, grades, size);
-        CreateGradeBinary(bin_g, grades, size);
-
-        fin_s.close();
-        fin_g.close();
-        bin_s.close();
-        bin_g.close();
-
-
-    }
-    catch (const char* msg) {
-        std::cerr << msg;
-    }
-}
