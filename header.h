0e2309ddb5bb871c0a3533daa59453f6d8dc4452
AlexIskandarr <senior.sasha11@gmail.com>
2025-09-07 23:23:49 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,4 +1,5 @@
 
+
 #ifndef HEADER_H
 #define HEADER_H
 #include <iostream>
@@ -12,12 +13,11 @@ void CheckInputFile(std::ifstream&);
 void CheckOutputFile(std::fstream&);
 int32_t CountStudents(std::ifstream&);
 void InputStudents(std::ifstream&, std::string*, int32_t);
-void CreateStudentBinary(std::fstream&, std::string*, int32_t);
+void CreateBinary(std::fstream&, std::string*, int32_t);
 void InputGrades(std::ifstream&, std::string*, int32_t);
-void CreateGradeBinary(std::fstream&, std::string*, int32_t);
+void MergeStudentDataWithGrades(std::string*, std::string*, int32_t);
 double calculateAverage(const std::string&, std::string&, std::string&, std::string&);
 void processFile(const std::string&, const std::string&);
-void CreateListWithFools(std::string*&, int32_t);
 void CreateArray(std::ifstream&, std::string*&, int32_t);
 std::string* CreateFoolsArray(std::string*&, int32_t, int32_t);
 int32_t CountFools(std::string*&, int32_t);
@@ -28,11 +28,10 @@ void myQuickSort(int32_t*, int32_t, int32_t, const int32_t*, const std::string*)
 void SortIndices(int32_t, int32_t*&, const int32_t*, const std::string*);
 void CreateSortedFoolsBin(const std::string*, const int32_t*, int32_t);
 std::string* CreateGroupArray(std::string*&, int32_t, int32_t, int32_t);
-bool СompareBySurname(const std::string&, const std::string&);
-void SortGroupBySurnames(std::string*&, int32_t);
+bool CompareBySurname(const std::string&, const std::string&);
+void SortGroup(std::string*&, int32_t);
 void CreateSortedGroupList(std::string*&, int32_t);
 bool CompareByAverageDescending(const std::string&, const std::string&);
-void SortGroupByAverageDescending(std::string*&, int32_t);
 void CreateSortedGroupAverageList(std::string*&, int32_t);
 std::string* CreateGeniusArray(std::string*&, int32_t, int32_t);
 int32_t CountGenius(std::string*&, int32_t);
@@ -41,4 +40,5 @@ void CreateSortedList(const std::string*, const int32_t*, int32_t, const std::st
 int32_t CountRecordsInBinaryFile(std::ifstream& fin);
 void SortList(const char*, const char*);
 #endif
+
 
